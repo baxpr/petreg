f6daf37c3a8777968941bde657cbf7a9ae71d5fd
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-05-07 18:30:10 -0500

    comments

--- src/register.sh
+++ src/register.sh
@@ -20,29 +20,34 @@
 
 # Copy files to outdir / working dir. Use convenient filenames so
 # we can just hardcode the filenames below.
+echo "Copying inputs"
 cp ${pet_niigz} ${out_dir}/pet.nii.gz
 cp ${ct_niigz} ${out_dir}/ct.nii.gz
 cp ${mr_niigz} ${out_dir}/mr.nii.gz
 cp ${seg_niigz} ${out_dir}/seg.nii.gz
-
-# Change to working dir (saves typing below)
 cd ${out_dir}
 
-# Within-series registration for PET
+# Initial PET motion correction
+echo "Within-series PET registration"
 mcflirt -in pet -out rpet -meanvol -plots
 
 # Register mean PET to CT (usesqform is crucial)
+echo "Register mean PET to CT"
 flirt -usesqform -dof 6 -in rpet_mean_reg -ref ct -omat rpet_to_ct.mat
 
 # Register CT to MR (usesqform is crucial)
+echo "Register CT to MR"
 flirt -usesqform -dof 6 -cost normmi -in ct -ref mr -out mct -omat ct_to_mr.mat
 
 # Combine PET-CT and CT-MR transforms
 convert_xfm -omat rpet_to_mr.mat -concat ct_to_mr.mat rpet_to_ct.mat
 
 # Apply registration to PET means and PET series (to MR space)
+echo "Apply registration"
 flirt -in rpet_mean_reg -ref mr -init rpet_to_mr.mat -applyxfm -out mrpet_mean_reg
 flirt -in rpet -ref mr -init rpet_to_mr.mat -applyxfm -out mrpet
 
 # Extract PET values from seg (multiatlas) regions
-#fslstats -t -K seg mrpet -c -v -m
+echo "Extract regional values"
+fslstats -t -K seg mrpet -c -v -m
+
