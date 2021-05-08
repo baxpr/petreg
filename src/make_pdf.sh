13ec17c6200d4bba3ce0e4521efb72a26cd34bbc
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-05-07 21:39:35 -0500

    pdf

---
+++ src/make_pdf.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+echo "Making PDF"
+
+# FSL init
+PATH=${FSLDIR}/bin:${PATH}
+. ${FSLDIR}/etc/fslconf/fsl.sh
+
+# Work in output directory
+cd ${out_dir}
+
+# Timestamp
+thedate=$(date)
+
+# ROI overlaid on mean PET, near PET COM
+com=( $(fslstats mrpet_mean_reg -c) )
+com[0]=$(echo ${com[0]} + 10 | bc)
+fsleyes render -of petroi.png \
+	--scene ortho --worldLoc ${com[@]} --displaySpace world \
+    --size 1800 600 --xzoom 1000 --yzoom 1000 --zzoom 1000 \
+	--layout horizontal --hideCursor \
+	mrpet_mean_reg --overlayType volume \
+	seg --overlayType label --outline --outlineWidth 1 --lut random_big
+
+# Same but CT underlay
+fsleyes render -of ctroi.png \
+	--scene ortho --worldLoc ${com[@]} --displaySpace world \
+    --size 1800 600 --xzoom 1000 --yzoom 1000 --zzoom 1000 \
+	--layout horizontal --hideCursor \
+	mct --overlayType volume \
+	seg --overlayType label --outline --outlineWidth 1 --lut random_big
+
+
+# Combine
+info_string="$project $subject $session $scan"
+
+convert -size 2600x3365 xc:white \
+	-gravity center \( petroi.png -geometry '2400x2400+0-0' \) -composite \
+	-gravity center -pointsize 48 -annotate +0-1250 \
+	"MR ROIs on mean PET" \
+	-gravity center \( ctroi.png -geometry '2400x2400+0+1200' \) -composite \
+	-gravity center -pointsize 48 -annotate +0-50 \
+	"MR ROIs on CT" \
+	-gravity SouthEast -pointsize 48 -annotate +100+100 "${thedate}" \
+	page1.png
+
+convert page1.png petreg.pdf
+
