aa1c8b63cd9c535f8363e03d9ab3eb65c3b00d95
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-05-07 19:41:22 -0500

    reformat

--- src/reformat_extracted.py
+++ src/reformat_extracted.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python
 
+import argparse
 import sys
 import pandas
 
-labels_txt = sys.argv[1]
-vals_txt = sys.argv[2]
+p = argparse.ArgumentParser()
+p.add_argument('--out_csv', help='Output csv', required=True)
+p.add_argument('--labels_txt', help='Region labels', required=True)
+p.add_argument('--vals_txt', help='Regional values from fslmeants', required=True)
+a = p.parse_args()
 
-rois = pandas.read_csv(labels_txt,header=None)
-print(rois)
-print(rois.loc[:,0].values)
-data = pandas.read_csv(vals_txt,header=None,delimiter='\s+')
+# ROI labels from csv file. No header, numbering must match
+# the ROI image and fslmeants count. Labels must be unique
+rois = pandas.read_csv(a.labels_txt,header=None,names=['Label','Region'])
 
-print(data)
+# Read space-delimited fslmeants text file
+data = pandas.read_csv(a.vals_txt,header=None,delimiter='\s+')
+
+# Drop columns of all zero (missing regions)
+# https://stackoverflow.com/a/21165116
+keeps = (data != 0).any(axis=0)
+data = data.loc[:, keeps]
+rois = rois.loc[keeps,:]
+
+# Use ROI labels as dataframe column names
+data = data.set_axis(rois['Region'],axis=1)
+
+# Save to file
+data.to_csv(a.out_csv,index=False)
