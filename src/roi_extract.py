1cf2a4f6d0717515e96f7ee1e2bb73bc5c350762
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-05-10 21:20:44 -0500

    extract

--- src/roi_extract.py
+++ src/roi_extract.py
@@ -4,24 +4,22 @@ import argparse
 import nibabel
 import nilearn.regions
 import pandas
+import numpy
 
 p = argparse.ArgumentParser()
 p.add_argument('--labels_niigz', help='ROI label image', required=True)
-p.add_argument('--labels_csv', help='ROI label csv', required=False)
+p.add_argument('--labels_csv', help='ROI label csv', required=True)
 p.add_argument('--data_niigz', help='Time series data to extract', required=True)
 a = p.parse_args()
 
-# FLIRT is resampling with not quiiiiite identical affines (error 1.5e-8). Try -anglerep quaternion ?
-# No, the problem is a discrepancy between seg and mr. SLANT problem. For now we can resample to seg geom.
-data = nibabel.load(a.data_niigz)
-labels = nibabel.load(a.labels_niigz)
-print(data.affine)
-print(' ')
-print(labels.affine)
-print(' ')
-print(f'Max difference: {abs(data.affine-labels.affine).max():7.3}')
+means,labels = nilearn.regions.img_to_signals_labels(a.data_niigz,a.labels_niigz,strategy='mean')
+sdevs,_ = nilearn.regions.img_to_signals_labels(a.data_niigz,a.labels_niigz,strategy='standard_deviation')
 
-means = nilearn.regions.img_to_signals_labels(a.data_niigz,a.labels_niigz,strategy='mean')
+label_list = pandas.read_csv(a.labels_csv,header=None)
+textlabels = label_list[1][label_list[0].searchsorted(labels)]
 
-print(means)
+means = pandas.DataFrame(means,columns=textlabels)
+sdevs = pandas.DataFrame(sdevs,columns=textlabels)
 
+means.to_csv('means.csv',index=False)
+sdevs.to_csv('sdevs.csv',index=False)
