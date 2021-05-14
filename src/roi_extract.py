b02a53f9b68c140cabf6629837c176379d72fff2
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-05-13 21:30:31 -0500

    labels

--- src/roi_extract.py
+++ src/roi_extract.py
@@ -14,14 +14,19 @@ p.add_argument('--data_niigz', help='Time series data to extract', required=True
 p.add_argument('--out_dir', help='Output directory', required=True)
 a = p.parse_args()
 
+# nilearn region of interest signal extraction
 means,labels = nilearn.regions.img_to_signals_labels(a.data_niigz,a.labels_niigz,strategy='mean')
 sdevs,_ = nilearn.regions.img_to_signals_labels(a.data_niigz,a.labels_niigz,strategy='standard_deviation')
 
-label_list = pandas.read_csv(a.labels_csv,header=None)
-textlabels = label_list[1][label_list[0].searchsorted(labels)]
+# Round the labels we got from the image to nearest int, then match the correct
+# region name to each using pandas merge with the separate label csv
+label_list = pandas.read_csv(a.labels_csv)
+dlabels = pandas.DataFrame([int(x) for x in labels],columns=['Label'])
+textlabels = dlabels.merge(label_list,how='left',on='Label')
 
-means = pandas.DataFrame(means,columns=textlabels)
-sdevs = pandas.DataFrame(sdevs,columns=textlabels)
+# Save to file with the region names as column names
+means = pandas.DataFrame(means,columns=textlabels.Region)
+sdevs = pandas.DataFrame(sdevs,columns=textlabels.Region)
 
 means.to_csv(os.path.join(out_dir,'means.csv'),index=False)
 sdevs.to_csv(os.path.join(out_dir,'sdevs.csv'),index=False)
