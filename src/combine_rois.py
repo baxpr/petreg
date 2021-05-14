0fdb13aaff6275bd71bccacd8cfa7574c0991afd
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-05-13 19:29:07 -0500

    new label method

--- src/combine_rois.py
+++ src/combine_rois.py
@@ -5,29 +5,11 @@ import nibabel
 import numpy
 import pandas
 
-# New ROIs from multiatlas labels
-rois = {
-    'Hippocampus': [47,48],
-    'Parahippocampus': [170,171],
-    'Putamen': [57, 58],
-    'InferiorParietal': [194,195,106,107]
-    'Precuneus': [168,169]
-    'AnteriorCingulate': [100,101]
-    'Cingulate': [138,139]
-    'PosteriorCingulate': [166,167]
-    'Fusiform': [122,123]
-    'Thalamus': [59,60]
-    'Entorhinal': [116,117]
-    'Insula': [102,103,172,173]
-    'FrontalLobe': [152,153,190,191,142,143,162,163,164,165,204,205,150,151,182,183,190,191,140,141,136,137,146,147,104,105, 178, 179, 186, 187, 192, 193]
-    'Cerebellum': (1, [38,39,40,41,71,72,73] ),
-    'Ventricle':  (2, [4,11,49,50,51,52]     ),
-}
-
 # Parse inputs
 p = argparse.ArgumentParser()
 p.add_argument('--seg_niigz', help='Multiatlas segmentation', required=True)
-p.add_argument('--out_pfx', help='Output prefix', required=True)
+p.add_argument('--groups_csv', help='Multiatlas ROI groupings', required=True)
+p.add_argument('--out_pfx', help='Output prefix, including path', required=True)
 a = p.parse_args()
 
 # Read our seg image
@@ -35,15 +17,24 @@ img_seg = nibabel.load(a.seg_niigz)
 data_seg = numpy.around(img_seg.get_fdata())
 affine_seg = img_seg.affine
 
+# Load the groupings, drop ungrouped, and identify unique groups
+groups = pandas.read_csv(a.groups_csv)
+groups = groups.loc[groups.Group.notna(),:]
+rois = groups.Group.unique()
+rois.sort()
+
 # Create the refactored ROI image and its corresponding label file
 data_roi = numpy.zeros(img_seg.shape)
 Label = list()
 Region = list()
+k = 0
 for roi in rois:
+    k = k + 1
     Region.append(roi)
-    Label.append(rois[roi][0])
-    ind = numpy.isin(data_seg,rois[roi][1])
-    data_roi[ind] = rois[roi][0]
+    Label.append(k)
+    matchlabels = groups.Label[numpy.isin(groups.Group,roi)]
+    ind = numpy.isin(data_seg,matchlabels)
+    data_roi[ind] = k
 
 labels_roi = pandas.DataFrame(list(zip(Label, Region)),columns =['Label', 'Region'])
 labels_roi.to_csv(f'{a.out_pfx}.csv',index=False)
