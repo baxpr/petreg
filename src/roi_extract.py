3fc7c34d21b822da37e2190f49e04667a4dbdec2
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-05-14 19:52:14 -0500

    3d

--- src/roi_extract.py
+++ src/roi_extract.py
@@ -14,9 +14,23 @@ p.add_argument('--data_niigz', help='Time series data to extract', required=True
 p.add_argument('--out_pfx', help='Output directory and prefix', required=True)
 a = p.parse_args()
 
-# nilearn region of interest signal extraction
-means,labels = nilearn.regions.img_to_signals_labels(a.data_niigz,a.labels_niigz,strategy='mean')
-sdevs,_ = nilearn.regions.img_to_signals_labels(a.data_niigz,a.labels_niigz,strategy='standard_deviation')
+# Region of interest signal extraction. We need a hack to handle 3D image, since
+# nilearn method only works for 4D.
+data = nibabel.load(a.data_niigz)
+if data.ndim==3:
+    means,labels = nilearn.regions.img_to_signals_labels(
+        [a.data_niigz,a.data_niigz],a.labels_niigz,strategy='mean')
+    sdevs,_ = nilearn.regions.img_to_signals_labels(
+        [a.data_niigz,a.data_niigz],a.labels_niigz,strategy='standard_deviation')
+    means = means[0,:]
+    means = numpy.reshape(means,(1,-1))
+    sdevs = sdevs[0,:]
+    sdevs = numpy.reshape(sdevs,(1,-1))
+else:
+    means,labels = nilearn.regions.img_to_signals_labels(
+        a.data_niigz,a.labels_niigz,strategy='mean')
+    sdevs,_ = nilearn.regions.img_to_signals_labels(
+        a.data_niigz,a.labels_niigz,strategy='standard_deviation')
 
 # Round the labels we got from the image to nearest int, then match the correct
 # region name to each using pandas merge with the separate label csv
