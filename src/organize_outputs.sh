6e9efdab38ff0b919f12eec23350b0b9646b1047
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-06-02 15:47:17 -0400

    outputs

--- src/organize_outputs.sh
+++ src/organize_outputs.sh
@@ -15,22 +15,22 @@ mkdir PET_ROI_SDEVS
 cp mrpet_sdevs.csv PET_ROI_SDEVS
 
 mkdir SUM_REG
-cp mrpet_sum.nii.gz SUMPET_REG
+cp mrpet_sum.nii.gz SUM_REG
 
 mkdir SUM_ROI_MEANS
-cp mrpet_sum_means.csv SUMPET_ROI_MEANS
+cp mrpet_sum_means.csv SUM_ROI_MEANS
 
 mkdir SUM_ROI_SDEVS
-cp mrpet_sum_sdevs.csv SUMPET_ROI_SDEVS
+cp mrpet_sum_sdevs.csv SUM_ROI_SDEVS
 
 mkdir SUVR_REG
-cp mrpet_suvr.nii.gz SUMPET_SUVR_REG
+cp mrpet_suvr.nii.gz SUVR_REG
 
 mkdir SUVR_ROI_MEANS
-cp mrpet_suvr_means.csv SUMPET_SUVR_ROI_MEANS
+cp mrpet_suvr_means.csv SUVR_ROI_MEANS
 
 mkdir SUVR_ROI_SDEVS
-cp mrpet_suvr_sdevs.csv SUMPET_SUVR_ROI_SDEVS
+cp mrpet_suvr_sdevs.csv SUVR_ROI_SDEVS
 
 mkdir MEANPET_REG
 cp mrpet_mean_reg.nii.gz MEANPET_REG
